2332c3a6ca9a7799ac5d2ea122f8e34f878aa7ca
nutszebra <nutszebra@gmail.com>
2017-09-06 20:17:43 +0900

    log and optimizer

---
+++ log.py
@@ -0,0 +1,13 @@
+import utility
+
+
+class Log(object):
+
+    def __init__(self, save_path, name):
+        self.save_path, self.name = save_path, name
+
+    def write(self, seq, debug=None):
+        if debug is not None:
+            print('{}: {}'.format(debug, seq))
+        with open('{}/{}'.format(self.save_path, self.name), 'a') as f:
+            f.write('{}\n'.format(seq))
---
+++ optimizers.py
@@ -0,0 +1,24 @@
+import torch.optim as optim
+import utility
+
+
+class MomentumSGD(object):
+
+    def __init__(self, model, lr, momentum, schedule=[10, 20], lr_decay=0.1):
+        self.model, self.lr, self.momentum = model, lr, momentum
+        self.schedule, self.lr_decay = schedule, lr_decay
+        self.optimizer = optim.SGD(model.parameters(), lr=lr, momentum=momentum)
+
+    def __call__(self, i):
+        if i in self.schedule:
+            for p in self.optimizer.param_groups:
+                previous_lr = p['lr']
+                new_lr = p['lr'] * self.lr_decay
+                print('{}->{}'.format(previous_lr, new_lr))
+                p['lr'] = new_lr
+
+    def step(self):
+        self.optimizer.step()
+
+    def zero_grad(self):
+        self.optimizer.zero_grad()
