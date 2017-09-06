75221630c758cf21c721971d1f6d5e56eb8aee93
nutszebra <nutszebra@gmail.com>
2017-09-07 03:35:43 +0900

    retrain_graph=True

--- hvp.py
+++ hvp.py
@@ -4,7 +4,7 @@ from torch.autograd import grad
 
 
 def hvp(y, w, v):
-    first_grads = grad(y, w, create_graph=True)
+    first_grads = grad(y, w, retain_graph=True, create_graph=True)
     grad_v = 0
     for g, v in six.moves.zip(first_grads, v):
         grad_v += torch.sum(g * v)
