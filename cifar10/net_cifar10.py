f245c8c1c2f531476ef25950be34bfcc36dc219e
nutszebra <nutszebra@gmail.com>
2017-09-08 21:43:14 +0900

    removed print

--- cifar10/net_cifar10.py
+++ cifar10/net_cifar10.py
@@ -14,12 +14,10 @@ class Net(nn.Module):
         self.name = 'cifar10'
 
     def forward(self, x):
-        print('debug')
         x = F.relu(F.max_pool2d(self.conv1(x), 2))
         x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
         x = x.view(-1, 500)
         x = F.relu(self.fc1(x))
         x = F.dropout(x, training=self.training)
         x = self.fc2(x)
-        print('debug')
         return F.log_softmax(x)
