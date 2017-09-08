df6262fe865ae67fe16d2cea2c99d5d52bd5aa4f
nutszebra <nutszebra@gmail.com>
2017-09-08 21:40:45 +0900

    modified network

--- cifar10/net_cifar10.py
+++ cifar10/net_cifar10.py
@@ -6,7 +6,7 @@ class Net(nn.Module):
 
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+        self.conv1 = nn.Conv2d(3, 10, kernel_size=5)
         self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
         self.conv2_drop = nn.Dropout2d()
         self.fc1 = nn.Linear(500, 50)
