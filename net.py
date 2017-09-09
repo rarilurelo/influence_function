541b17d5f874f33231b42650f3a40ea5e6cced0a
nutszebra <nutszebra@gmail.com>
2017-09-09 23:03:17 +0900

    modified network

--- net.py
+++ net.py
@@ -17,7 +17,8 @@ class Net(nn.Module):
     def forward(self, x):
         x = F.relu(F.max_pool2d(self.conv1(x), 2))
         x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-        x = x.view(-1, 320)
+        batch, _, _, _ = x.data.shape
+        x = x.view(batch, 320)
         x = F.relu(self.fc1(x))
         x = F.dropout(x, training=self.training)
         x = self.fc2(x)
