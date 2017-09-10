a0e9ef18843d3386263a07b0782d3a54b28552a5
nutszebra <nutszebra@gmail.com>
2017-09-10 17:31:05 +0900

    forgot to use weight decay

--- optimizers.py
+++ optimizers.py
@@ -6,8 +6,8 @@ class MomentumSGD(object):
 
     def __init__(self, model, lr, momentum, schedule=[10, 20], lr_decay=0.1, weight_decay=1.0e-4):
         self.model, self.lr, self.momentum = model, lr, momentum
-        self.schedule, self.lr_decay = schedule, lr_decay
-        self.optimizer = optim.SGD(model.parameters(), lr=lr, momentum=momentum)
+        self.schedule, self.lr_decay, self.weight_decay = schedule, lr_decay, weight_decay
+        self.optimizer = optim.SGD(model.parameters(), lr=lr, momentum=momentum, weight_decay=weight_decay)
 
     def __call__(self, i):
         if i in self.schedule:
@@ -16,6 +16,7 @@ class MomentumSGD(object):
                 new_lr = p['lr'] * self.lr_decay
                 print('{}->{}'.format(previous_lr, new_lr))
                 p['lr'] = new_lr
+            self.lr = new_lr
             self.info()
 
     def step(self):
